68c6dcc331e7d65c2ddc5505e87e9a069adef92a
timothy-kumah <timothy.kumah@amalitech.com>
2025-01-10 15:25:31 +0000

    fix role

--- task-manager-app/functions/tasks/get_all_tasks.py
+++ task-manager-app/functions/tasks/get_all_tasks.py
@@ -5,7 +5,7 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TasksTable')
 
 def lambda_handler(event, context):
-    user_role = event['requestContext']['authorizer']['claims']['role']
+    user_role = event['requestContext']['authorizer']['claims']['cognito:groups']
     if user_role != 'admin':
         return {'statusCode': 403, 'body': 'Unauthorized'}
 
